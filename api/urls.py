404d2fd9646b804fb4a0ca771e2ea918904c4ecc
mdallrabbi <dreamer.rabbi@gmail.com>
2021-11-22 00:45:52 +0600

    PSQL added

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Overview, name="overview"),
-    path('list/', views.List, name="list"),
+    path('list/', views.List, name="todoList"),
     path('detail/<str:pk>/', views.Detail, name="detail"),
     path('update/<str:pk>/', views.Update, name="update"),
     path('create/', views.Create, name="create"),
